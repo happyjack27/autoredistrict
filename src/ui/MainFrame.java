f6fc0f34b303739265e13a3108ea94ebc7ff8be5
jimbrill <jimbrill@jims-air>
2014-11-29 14:14:28 -0600

    a

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -9,10 +9,26 @@ import java.util.*;
 
 import javax.swing.*;
 
+import mapCandidates.Ecology;
+import mapCandidates.Settings;
+
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+
 
 public class MainFrame extends JFrame {
-	private JTextField textField;
-	private JTextField textField_1;
+	JTextField textField_1 = new JTextField();
+	JTextField textField = new JTextField();
+	JSlider slider = new JSlider();
+	JSlider slider_1 = new JSlider();
+	JSlider slider_2 = new JSlider();
+	JSlider slider_3 = new JSlider();
+	JSlider slider_4 = new JSlider();
+	JSlider slider_5 = new JSlider();
+	JSlider slider_6 = new JSlider();
+	JSlider slider_7 = new JSlider();
+
+	public Ecology ecology = new Ecology();
 	public MainFrame() {
 		Dimension d = new Dimension(800,1024);
 		//this.setPreferredSize(d);
@@ -48,37 +64,76 @@ public class MainFrame extends JFrame {
 		lblPopulationReplaced.setBounds(6, 6, 172, 16);
 		panel.add(lblPopulationReplaced);
 		
-		JSlider slider = new JSlider();
+		slider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				Settings.replace_fraction = slider.getValue()/100.0;
+			}
+		});
 		slider.setBounds(6, 27, 190, 29);
 		panel.add(slider);
-		
 		JLabel lblBorderMutation = new JLabel("% border mutation");
 		lblBorderMutation.setBounds(6, 68, 172, 16);
 		panel.add(lblBorderMutation);
+		slider_1.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				Settings.mutation_boundary_rate = slider.getValue()/100.0;
+			}
+		});
 		
-		JSlider slider_1 = new JSlider();
 		slider_1.setBounds(6, 89, 190, 29);
 		panel.add(slider_1);
 		
 		JLabel lblScatterMutation = new JLabel("% scatter mutation");
 		lblScatterMutation.setBounds(6, 130, 172, 16);
 		panel.add(lblScatterMutation);
+		slider_2.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				Settings.mutation_rate = slider.getValue()/100.0;
+			}
+		});
 		
-		JSlider slider_2 = new JSlider();
 		slider_2.setBounds(6, 151, 190, 29);
 		panel.add(slider_2);
 		
 		JLabel lblPopulation = new JLabel("Population");
 		lblPopulation.setBounds(6, 192, 104, 16);
 		panel.add(lblPopulation);
+		textField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				textField.postActionEvent();
+			}
+		});
+		textField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 try {
+					 Settings.population = new Integer(textField.getText());
+				 } catch (Exception ex) {
+					 
+				 }
+			}
+		});
 		
-		textField = new JTextField();
 		textField.setText("512");
 		textField.setBounds(105, 186, 91, 28);
 		panel.add(textField);
 		textField.setColumns(10);
+		textField_1.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				textField_1.postActionEvent();
+			}
+		});
+		textField_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 try {
+					 Settings.trials = new Integer(textField.getText());
+				 } catch (Exception ex) {
+					 
+				 }
+			}
+		});
 		
-		textField_1 = new JTextField();
 		textField_1.setText("128");
 		textField_1.setColumns(10);
 		textField_1.setBounds(105, 220, 91, 28);
@@ -91,36 +146,57 @@ public class MainFrame extends JFrame {
 		JLabel lblCompactness = new JLabel("compactness");
 		lblCompactness.setBounds(6, 315, 172, 16);
 		panel.add(lblCompactness);
+		slider_3.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				Settings.geometry_weight = slider_3.getValue()/100.0;
+			}
+		});
 		
-		JSlider slider_3 = new JSlider();
 		slider_3.setBounds(6, 336, 190, 29);
 		panel.add(slider_3);
 		
 		JLabel lblPopulationBalance = new JLabel("population balance");
 		lblPopulationBalance.setBounds(6, 377, 172, 16);
 		panel.add(lblPopulationBalance);
+		slider_4.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				Settings.population_balance_weight = slider_3.getValue()/100.0;
+			}
+		});
 		
-		JSlider slider_4 = new JSlider();
 		slider_4.setBounds(6, 398, 190, 29);
 		panel.add(slider_4);
 		
 		JLabel lblProportionalRepresentation = new JLabel("proportional representation\n");
 		lblProportionalRepresentation.setBounds(6, 439, 172, 16);
 		panel.add(lblProportionalRepresentation);
+		slider_5.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				Settings.disenfranchise_weight = slider_5.getValue()/100.0;
+
+			}
+		});
 		
-		JSlider slider_5 = new JSlider();
 		slider_5.setBounds(6, 460, 190, 29);
 		panel.add(slider_5);
 		
 		JLabel lblVotingPowerBalance = new JLabel("voting power balance");
 		lblVotingPowerBalance.setBounds(6, 501, 172, 16);
 		panel.add(lblVotingPowerBalance);
+		slider_6.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				Settings.voting_power_balance_weight = slider_3.getValue()/100.0;
+			}
+		});
 		
-		JSlider slider_6 = new JSlider();
 		slider_6.setBounds(6, 522, 190, 29);
 		panel.add(slider_6);
+		slider_7.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				Settings.disconnected_population_weight = slider_3.getValue()/100.0;
+			}
+		});
 		
-		JSlider slider_7 = new JSlider();
 		slider_7.setBounds(6, 583, 190, 29);
 		panel.add(slider_7);
 		
