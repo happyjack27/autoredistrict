bd58b489d6806f72fce9378fff2ee5f37ddc0f6a
jimbrill <jimbrill@jims-air>
2014-11-28 20:36:30 -0600

    a

---
+++ src/ui/Applet.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javax.swing.*;
+
+public class Applet extends JApplet {
+    public static void main( String[] args ) {
+		new Applet();
+	}
+    public Applet() {
+    	new MainFrame();
+    }
+
+
+}
---
+++ src/ui/MainFrame.java
@@ -0,0 +1,15 @@
+package ui;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import java.io.*;
+
+import java.util.*;
+
+import javax.swing.*;
+
+
+public class MainFrame extends JFrame {
+
+}
