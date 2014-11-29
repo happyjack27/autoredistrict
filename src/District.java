1fef97ba29178c593780450f28a8213a14e01ce1
jimbrill <jimbrill@jims-air>
2014-11-28 20:25:38 -0600

    g

--- src/District.java
+++ src/District.java
@@ -1,4 +1,5 @@
-//buisness objects
+import java.util.*;
+
 class District {
     Vector<Block> blocks = new Vector<Block>();
     public double[] wins;
@@ -6,8 +7,10 @@ class District {
 
     double getPopulation() {
         double pop = 0;
-        for( Block block : blocks)
-              pop += block.population;
+        for( Block block : blocks) {
+        	for(double p : block.population)
+              pop += p;
+        }
         return pop;
     }
 
@@ -103,66 +106,12 @@ class District {
     }
     double getRegionPopulation(Vector<Block> region) {
         double population = 0;
-        for( Block block : region)
-            population += block.population;
+        for( Block block : region) {
+        	for(double p : block.population)
+                population += p;
+        }
         return population;
     }
 
 
-}
-class Block {
-    int index;
-    double[] population;
-    double[] prob_turnout;
-    double[][] prob_vote = null;//new double[DistrictMap.candidates.size()];
-    double[] vote_cache = null;
-    double[][] vote_caches = null;
-    static boolean use_vote_caches = true;
-    static boolean use_vote_cache = true;
-    static int cache_reuse_times = 16;
-    static int vote_cache_size = 128;
-    int cache_reused = 0;
-
-    Vector<Edge> edges = new Vector<Edge>();
-    double[] getVotes() {
-        if( use_vote_caches) {
-            if( vote_caches == null) {
-                vote_caches = new double[vote_cache_size][];
-                for( int i = 0; i < vote_caches.length; i++) {
-                    generateVotes();
-                    vote_caches[i] = vote_cache;
-                }
-            }
-            return vote_caches[((int)Math.random()*(double)vote_caches.length)];
-        } else if( vote_cache == null || cache_reused >= cache_reuse_times) {
-            generateVotes();
-            cache_reuse_times = 0;
-        }
-        cache_reuse_times++;
-        return vote_cache;
-    }
-
-    void generateVotes() {
-        double[] votes = new double[prob_vote.length];
-        for(int i = 0; i < votes; i++) {
-            votes[i] = 0;
-        }
-        for(int h = 0; h < population.length; h++) {
-            for(int i = 0; i < population[h]; i++) {
-                double p = Math.random();
-                if( p > prob_turnout[h]) {
-                    continue;
-                }
-                p = Math.random();
-                for( int j = 0; j < prob_vote[h].length; j++) {
-                    p -= prob_vote[h][j];
-                    if( p <= 0) {
-                        votes[j]++;
-                        break;
-                    }
-                }
-            }
-        }
-    vote_cache = votes;
-    }
 }
