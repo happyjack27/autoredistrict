e36d7d9c250938f787758a0a88b4f6b46ed93c98
jimbrill <jimbrill@jims-air>
2014-11-30 10:34:39 -0600

    a

--- src/mapCandidates/Demographic.java
+++ src/mapCandidates/Demographic.java
@@ -1,5 +1,7 @@
 package mapCandidates;
 
+import java.util.Vector;
+
 import serialization.ReflectionJSONObject;
 
 public class Demographic extends ReflectionJSONObject<Demographic> {
@@ -7,4 +9,18 @@ public class Demographic extends ReflectionJSONObject<Demographic> {
 	public int population;
 	public double turnout_probability;
 	double[] vote_prob;
+	
+	public void post_deserialize() {
+		super.post_deserialize();
+		Vector<String> v_vote_prob = getVector("vote_probs");
+		vote_prob = new double[v_vote_prob.size()];
+		
+		for( int i = 0; i < v_vote_prob.size(); i++) {
+			vote_prob[i] = new Double(v_vote_prob.get(i));
+		}
+	}
+	public void pre_serialize() {
+		super.pre_serialize();
+		
+	}
 }
