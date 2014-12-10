34167b87a9d8c84c2c9be0c1fd9742c0b80824c6
jimbrill <jimbrill@jims-air>
2014-12-09 20:37:53 -0600

    a

--- src/geoJSON/Geometry.java
+++ src/geoJSON/Geometry.java
@@ -6,29 +6,45 @@ import java.util.*;
 
 public class Geometry extends ReflectionJSONObject<Geometry> {
 	public String type;
-	public double[][] coordinates;
+	public double[][][] coordinates;
 
 	@Override
 	public void post_deserialize() {
 		super.post_deserialize();
 		
 		if( containsKey("coordinates")) {
-			Vector<Vector<Object>> vvo = ((Vector<Vector<Vector<Object>>>)getVector("coordinates")).get(0);
-			coordinates = new double[vvo.size()][];
-			int j = 0;
-			for( int i = 0; i < vvo.size(); i++) {
+			Vector<Vector<Vector<Object>>> vvvo = (Vector<Vector<Vector<Object>>>)getVector("coordinates");
+			coordinates = new double[vvvo.size()][][];
+			int i2 = 0;
+			for( int i = 0; i < vvvo.size(); i++) {
 				try {
-				Vector<Object> vo = vvo.get(i);
-				coordinates[j] = new double[]{
-						Double.parseDouble((String)vo.get(0)),	
-						Double.parseDouble((String)vo.get(1)),	
-				};
-				j++;
+					Vector<Vector<Object>> vvo = vvvo.get(i);
+					coordinates[i2] = new double[vvo.size()][];
+					int k2 = 0;
+					for( int k = 0; k < vvo.size(); k++) {
+						try {
+							Vector<Object> vo = vvo.get(i);
+
+							coordinates[i2][k2] = new double[]{
+									Double.parseDouble((String)vo.get(0)),	
+									Double.parseDouble((String)vo.get(1)),
+							};
+						k2++;
+						} catch (Exception ex) { 
+							//System.out.println("ex "+vvo.get(i));
+						}
+					}
+					double[][] dd = new double[k2][];
+					for( int k = 0; i < dd.length; k++) {
+						dd[k] = coordinates[i2][k];
+					}
+					coordinates[i2] = dd;
+					i2++;
 				} catch (Exception ex) { 
 					//System.out.println("ex "+vvo.get(i));
 				}
 			}
-			double[][] dd = new double[j][];
+			double[][][] dd = new double[i2][][];
 			for( int i = 0; i < dd.length; i++) {
 				dd[i] = coordinates[i];
 			}
@@ -45,14 +61,16 @@ public class Geometry extends ReflectionJSONObject<Geometry> {
 		if( coordinates != null) {
 			Vector<Vector<String>> v = new Vector<Vector<String>>();
 
-			for( int i = 0; i < coordinates.length; i++) {
-				Vector v2 = new Vector();
-				v2.add(""+coordinates[i][0]);
-				v2.add(""+coordinates[i][1]);
-				v.add(v2);
-			}
 			Vector v3 = new Vector();
-			v3.add(v);
+			for( int j = 0; j < coordinates.length; j++) {
+				for( int i = 0; i < coordinates.length; i++) {
+					Vector v2 = new Vector();
+					v2.add(""+coordinates[j][i][0]);
+					v2.add(""+coordinates[j][i][1]);
+					v.add(v2);
+				}
+				v3.add(v);
+			}
 			put("coordinates",v3);
 		}
 		// TODO Auto-generated method stub
