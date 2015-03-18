a4d6f0b545d35a79441504345fb554a1f5120575
kbaas <happyjack27@gmail.com>
2015-03-18 14:52:11 -0500

    a

--- src/geoJSON/Project.java
+++ src/geoJSON/Project.java
@@ -1,11 +1,27 @@
 package geoJSON;
 
 import java.awt.Color;
+import java.util.*;
 
 import serialization.*;
 
 
 public class Project extends ReflectionJSONObject<Project> {
+	String source_file = "";
+	int number_of_districts = 10;
+	int members_per_district = 1;
+	boolean area_weigthed = true;
+	int initial_population = 128;
+	int simulated_elections = 4;
+	String population_column = "";
+	String result_column = "";
+	Vector<DemographicSet> demographics = new Vector<DemographicSet>();
+	String active_demographic_set = "";
+	double disconnected_weight = 0.5;
+	double population_balance_weight = 0.5;
+	double border_length_weight = 0.5;
+	double voting_power_weight = 0.5;
+	double representation_weight = 0.5;
 
 	public void post_deserialize() {
 		super.post_deserialize();
