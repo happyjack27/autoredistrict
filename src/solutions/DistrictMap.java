134bdfec765336e9ebea4e913de81513e5d8898c
kbaas <happyjack27@gmail.com>
2015-04-06 15:02:12 -0500

    a

--- src/solutions/DistrictMap.java
+++ src/solutions/DistrictMap.java
@@ -775,7 +775,8 @@ public class DistrictMap implements iEvolvable, Comparable<DistrictMap> {
         double weighted_sum = 0;
         for( int i = 0; i < lengths.length; i++) {
         	//weighted_sum += lengths[i] / Math.sqrt(areas[i]);
-        	weighted_sum += 1.0-((4.0*Math.PI*areas[i]) / (lengths[i]*lengths[i]));
+        	double isop = ((4.0*Math.PI*areas[i]) / (lengths[i]*lengths[i]));
+        	weighted_sum += 1.0 - (Settings.square_root_compactness ? Math.sqrt(isop) : isop);
         }
         return weighted_sum / (double)lengths.length;
     }
