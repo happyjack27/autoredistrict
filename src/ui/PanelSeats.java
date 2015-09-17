d8bad53d6092de2cc0d55d85e1565d2b5b3ca0b1
jimbrill <jimbrill@192.168.1.102>
2015-09-17 17:00:08 -0500

    a

---
+++ src/ui/PanelSeats.java
@@ -0,0 +1,85 @@
+package ui;
+
+import geography.FeatureCollection;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+import solutions.*;
+
+public class PanelSeats extends JPanel {
+	public MapPanel mapPanel;
+	public PanelSeats() {
+		this.setLayout(null);
+		this.setSize(new Dimension(50, 50));
+		this.setPreferredSize(new Dimension(50, 50));
+		
+	}
+    public void paintComponent(Graphics graphics0) {
+    	try {
+	    	Graphics2D g = (Graphics2D)graphics0;
+	        super.paintComponent(g);
+	        Dimension dim = this.getSize();
+	        if( mapPanel != null) {
+	        	dim = mapPanel.getSize();
+	        }
+	        double w = dim.width-10;
+	        
+	        if( mapPanel == null || mapPanel.featureCollection == null || mapPanel.featureCollection.ecology.population.size() == 0) {
+	        	return;
+	        }
+	        DistrictMap dm = mapPanel.featureCollection.ecology.population.get(0);
+	        double[] votes = new double[Candidate.candidates.size()];
+	        double[] seats = new double[Candidate.candidates.size()];
+	        double tot_votes = 0;
+	        double tot_seats = 0;
+	        for( District d : dm.districts) {
+	        	double[][] result = d.getElectionResults();
+	        	for( int i = 0; i < result[0].length; i++) {
+	        		votes[i] += result[0][i];
+	        		tot_votes += result[0][i];
+	        	}
+	        	for( int i = 0; i < result[1].length; i++) {
+	        		seats[i] += result[1][i];
+	        		tot_seats += result[1][i];
+	        	}
+	        }
+
+	        double x = 5;
+	        for( int i = 0; i < votes.length; i++) {
+	        	g.setColor(FeatureCollection.standard_district_colors[i]);
+	        	double width = w * (votes[i]/tot_votes);
+	        	g.fillRect((int)x, 30, (int)width, 15);
+	        	x += width;
+	        }
+	        
+	        double seat_width = w/tot_seats;
+	        
+	        x = 5;
+	        for( int i = 0; i < seats.length; i++) {
+		        if( seat_width > 5) {
+		        	for( int j = 0; j < seats[i]; j++) {
+			        	g.setColor(FeatureCollection.standard_district_colors[i]);
+			        	g.fillRect((int)x+1, 5, (int)seat_width-2, 15);
+		        		g.setColor(Color.black);
+			        	g.drawRect((int)x+1, 5, (int)seat_width-2, 15);
+			        	x += seat_width;
+		        	}
+	        	} else {
+		        	g.setColor(FeatureCollection.standard_district_colors[i]);
+		        	double width = w * (seats[i]/tot_seats);
+		        	g.fillRect((int)x, 5, (int)width, 15);
+		        	x += width;
+	        	}
+	        }
+	        
+    	} catch (Exception ex) {
+    		ex.printStackTrace();
+    	}
+    }
+
+}
