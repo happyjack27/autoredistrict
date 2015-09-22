5aafb5989d2aa635ed1d8b1ce804d22d87303dd8
kbaas <happyjack27@gmail.com>
2015-09-22 15:27:52 -0500

    a

--- src/solutions/Settings.java
+++ src/solutions/Settings.java
@@ -57,7 +57,7 @@ public class Settings extends serialization.ReflectionJSONObject<Settings> {
        	if( new_rate <= 0.0001) {
     		new_rate = 0.0001;
     	}
-    	double e = max_mutation*Math.exp(-0.000125*(double)generation); // reaches -0.0005 0.000005 at 4000
+    	double e = max_mutation*Math.exp(-0.0002*(double)generation); // reaches -0.0005 0.000005 at 4000
     	if( new_rate < e) {
     		new_rate = e;
     	}
