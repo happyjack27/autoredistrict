df97ffd19ef4c9e7b24773ce51912007f9af5463
kbaas <happyjack27@gmail.com>
2015-10-02 10:23:22 -0500

    a

--- src/ui/DialogSeatsVotesChart.java
+++ src/ui/DialogSeatsVotesChart.java
@@ -3,5 +3,38 @@ package ui;
 import javax.swing.*;
 
 public class DialogSeatsVotesChart extends JDialog {
-
+	public JPanel panel;
+	public JScrollPane scrollPane;
+	public JLabel lblVotes;
+	public JLabel lblSeats;
+	public JTable table;
+	//label.setUI(new VerticalLabelUI());
+	public DialogSeatsVotesChart() {
+		super();
+		initComponents();
+	}
+	private void initComponents() {
+		getContentPane().setLayout(null);
+		setTitle("Seats / Votes");
+		
+		panel = new JPanel();
+		panel.setBounds(40, 11, 200, 200);
+		getContentPane().add(panel);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(40, 257, 200, 200);
+		getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		lblVotes = new JLabel("Votes -->");
+		lblVotes.setBounds(40, 222, 61, 14);
+		getContentPane().add(lblVotes);
+		
+		lblSeats = new JLabel("Seats -->");
+		lblSeats.setUI(new VerticalLabelUI());
+		lblSeats.setBounds(10, 136, 20, 75);
+		getContentPane().add(lblSeats);
+	}
 }
