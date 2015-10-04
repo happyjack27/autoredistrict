21eeebb76be9695fa7ca9b473aced1e1fe11d460
jimbrill <jimbrill@192.168.1.102>
2015-10-03 21:27:08 -0500

    a

--- src/ui/FrameSeatsVotesChart.java
+++ src/ui/FrameSeatsVotesChart.java
@@ -14,6 +14,7 @@ import java.awt.event.ActionListener;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 
 public class FrameSeatsVotesChart extends JFrame {
@@ -146,6 +147,21 @@ public class FrameSeatsVotesChart extends JFrame {
 	    	}
 	    }
 	}
+	
+	public static Point lineIntersect(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+		   double denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+		   if (denom == 0.0) { // Lines are parallel.
+		      return null;
+		   }
+		   double ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3))/denom;
+		   double ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3))/denom;
+		     if (ua >= 0.0f && ua <= 1.0f && ub >= 0.0f && ub <= 1.0f) {
+		         // Get the intersection point.
+		         return new Point((int) (x1 + ua*(x2 - x1)), (int) (y1 + ua*(y2 - y1)));
+		     }
+
+		   return null;
+		  }
 	
 	private void initComponents() {
 		getContentPane().setLayout(null);
