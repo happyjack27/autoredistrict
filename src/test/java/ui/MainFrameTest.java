690751407336d9516e1b21d327144cef8a73c835
Gabe Heafitz <gabe.heafitz@gmail.com>
2024-03-06 17:35:58 -0500

    Test opening an ESRI shapefile
    Make sure it completes without throwing any exceptions.

--- src/test/java/ui/MainFrameTest.java
+++ src/test/java/ui/MainFrameTest.java
@@ -1,27 +1,23 @@
 package ui;
 
-import com.google.common.truth.*;
 import org.assertj.swing.core.BasicComponentFinder;
 import org.assertj.swing.core.ComponentFinder;
-import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
 import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.finder.JFileChooserFinder;
 import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.fixture.JFileChooserFixture;
+import org.assertj.swing.fixture.JMenuItemFixture;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
-import java.util.ArrayList;
+import java.io.File;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class MainFrameTest {
-
-    @BeforeAll
-    public static void setUpOnce() {
-        FailOnThreadViolationRepaintManager.install();
-    }
 
     FrameFixture frame;
 
@@ -61,5 +57,23 @@ class MainFrameTest {
 
         JMenu windowsMenu = (JMenu) menuBar.getComponent(4);
         assertThat(windowsMenu.getText()).isEqualTo("Windows");
+
+        // assert that the app can successfully open an ESRI shapefile without throwing any exceptions
+        try {
+            JMenuItem menuItem = fileMenu.getItem(4);
+
+            JMenuItemFixture openEsriShapefileMenuItem = new JMenuItemFixture(frame.robot(), menuItem);
+            openEsriShapefileMenuItem.click();
+
+            JFileChooserFixture fileChooserFixture = JFileChooserFinder.findFileChooser().withTimeout(1000).using(frame.robot());
+            File currentWorkingDirectory = new File(System.getProperty("user.dir"));
+            File sampleDataDirectory = new File(currentWorkingDirectory, "data/sample/delaware");
+            fileChooserFixture.setCurrentDirectory(sampleDataDirectory);
+            fileChooserFixture.selectFile(new File(sampleDataDirectory, "delaware.shp"));
+            fileChooserFixture.approve();
+
+        } catch (Exception e) {
+            fail(e);
+        }
     }
 }
